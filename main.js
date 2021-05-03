de1b70e1f59c8b5ab52a6b27388b662301b90ff5
vishva620545 <78867324+vishva620545@users.noreply.github.com>
2021-05-03 17:16:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(500,500);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('class114.png');
+}
